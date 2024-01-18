8d10e9bf61098884d07523911dc251eb8fe99502
arjun <arjunanoop101@gmail.com>
2024-01-18 16:08:21 +0530

    new changes added

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ let currentWeapon=0;
 let fighting;
 let monsterHealth;
 let inventory=["stick"];
+const locations=[
+    {name:"town square",
+     "button text":["Go to store","Go to cave","Fight dragon"],
+     "button functions":[goStore,goCave,fightDragon],
+     text:"You are in the town square. You see a sign that says \"Store\"."
 
+    },
+    {
+        name:"store",
+        "button text":["Buy 10 health (10 gold)","Buy weapon (30 gold)","Go to town square"],
+        "button functions":[buyHealth,buyWeapon,goTown],
+        text:"You enter the store."
+    }
+];
 const button1=document.querySelector("#button1");
 const button2=document.querySelector("#button2");
 const button3=document.querySelector("#button3");
@@ -18,26 +31,12 @@ const monsterStats=document.querySelector("#monsterStats");
 const monsterName=document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth")
 
-function goTown(){
-    button1.innerText="Go to store";
-    button2.innerText="Go to cave";
-    button3.innerText="Fight dragon";
-    text.innerText="You are in the town square. You see a sign that says Store."
-
-    button1.onclick=goStore;
-    button2.onclick=goCave;
-    button3.onclick=fightDragon;
+function goTown(){  
+    update(locations[0]);
 }
 
 function goStore(){
-    button1.innerText="Buy 10 health (10 gold)";
-    button2.innerText="Buy weapon (30 gold)";
-    button3.innerText="Go to town square";
-    text.innerText="You enter the store."
-
-    button1.onclick=buyHealth;
-    button2.onclick=buyWeapon;
-    button3.onclick=goTown;
+    update(locations[1]);
 }
 
 function goCave(){
@@ -54,6 +53,25 @@ function buyHealth(){
 
 function buyWeapon(){
 
+}
+
+function fightSlime(){
+
+}
+
+function fightBeast(){
+    
+}
+
+function update(location){
+    button1.innerText=location["button text"][0];
+    button2.innerText=location["button text"][1];
+    button3.innerText=location["button text"][2];
+    text.innerText=location.text;
+
+    button1.onclick=location["button functions"][0];
+    button2.onclick=location["button functions"][1];
+    button3.onclick=location["button functions"][2];
 }
 
 
