81d771e1ec26ab4c337c85bd14bd16c4fcdcefd3
arjun <arjunanoop101@gmail.com>
2024-01-26 10:40:35 +0530

    add final edits

--- script.js
+++ script.js
@@ -23,6 +23,30 @@ const weapons=[
         power:100
     }
 ];
+const monsters=[
+    {
+        name:"slime",
+        level:2,
+        health:15
+    },
+    {
+        name:"fanged beast",
+        level:8,
+        health:60
+
+    },
+    {
+        name:"dragon",
+        level:20,
+        health:300
+    },
+    {
+        name:"kill monster",
+        "button text":["Go to town square","Go to town square","Go to town square"],
+        "button functions":[goTown,goTown,goTown],
+        text:'The monster screams "Arg!"" as it dies. You gain experience points and find gold.'
+    }
+];
 const locations=[
     {name:"town square",
      "button text":["Go to store","Go to cave","Fight dragon"],
@@ -42,7 +66,28 @@ const locations=[
         "button functions":[fightSlime,fightBeast,goTown],
         text:"You enter the cave. You see some monsters."
 
-    }
+    },
+    
+    {
+        name:"fight",
+        "button text":["Attack","Dodge","Run"],
+        "button functions":[attack,dodge,goTown],
+        text:"You are fighting a monster."
+    },
+    {
+        name:"lose",
+        "button text":["REPLAY?","REPLAY?","REPLAY?"],
+        "button functions":[restart,restart,restart],
+        text:"You die. ☠️"
+    },
+    {
+        name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
+    
+      }
+    
 ];
 const button1=document.querySelector("#button1");
 const button2=document.querySelector("#button2");
@@ -69,8 +114,44 @@ function goCave(){
 }
 
 function fightDragon(){
-    console.log("Fighting dragon.");
+    fighting=2;
+    goFight();
 }
+function fightSlime(){
+    fighting=0;
+    goFight();
+}
+
+function fightBeast(){
+    fighting=1;
+    goFight();
+}
+function goFight(){
+    update(locations[3]);
+    monsterHealth=monsters[fighting].health;
+    monsterStats.style.display="block";
+    monsterName.innerText=monsters[fighting].name;
+    monsterHealthText.innerText=monsters[fighting].health;
+}
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText+=" You attack it with your "+weapons[currentWeapon].name +".";
+    health-=monsters[fighting].level;
+    monsterHealth-=weapons[currentWeapon].power+Math.floor(Math.random()*xp)+1;
+    healthText.innerText=health;
+    monsterHealthText.innerText=monsterHealth;
+    if(health<=0)
+    {
+        lose();
+    }
+    else if(monsterHealth<=0){
+        fighting===2 ? winGame():defeatMonster();
+    }
+}
+function dodge() {
+    text.innerText="You dodge the attack from the "+monsters[fighting].name;
+}
+
 
 function buyHealth(){
     if(gold>=10)
@@ -107,15 +188,10 @@ else{
 }
 }
 
-function fightSlime(){
 
-}
-
-function fightBeast(){
-
-}
 
 function update(location){
+    monsterStats.style.display="none";
     button1.innerText=location["button text"][0];
     button2.innerText=location["button text"][1];
     button3.innerText=location["button text"][2];
@@ -135,9 +211,40 @@ function sellWeapon()
         text.innerText="You sold a "+currentWeapon+".";
         text.innerText += " In your inventory you have: "+inventory;
     }
+    else
+    {
+        text.innerText="Don't sell your only weapon!";
+    }
 }
 
+function defeatMonster()
+{
+    gold+=Math.floor(monsters[fighting].level*6.7);
+    xp+=monsters[fighting].level;
+    goldText.innerText=gold;
+    xpText.innerText=xp;
+    update(locations[4]);
+}
+function lose()
+{
+    update(locations[5]);
+}
 
+function winGame(){
+    update(locations[6]);
+}
+
+function restart(){
+    xp=0;
+    health=100;
+    gold=50;
+    currentWeapon=0;
+    inventory=['stick'];
+    goldText.innerText=gold;
+    healthText.innerText=health;
+    xpText.innerText=xp;
+    goTown();
+}
 // initialize buttons.
 button1.onclick=goStore();
 button2.onclick=goCave();
